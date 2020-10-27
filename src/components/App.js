0d81b5fe3ecfc26b5dd4ff709bed145219e3a03c
JovicaSusa <jovicasusa1985@gmail.com>
2020-10-27 11:45:04 +0100

    Display letters when spelling

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 class App extends React.Component {
-  state = { term: "" }
+  state = { term: "", currentLetter: "" }
 
-  handleMe = event => {
+  handleMe = async event => {
     event.preventDefault();
-    let trimmedUppercasedTerm = this.state.term.replace(/\s+/g, '').toUpperCase();
+    let trimmedTerm = this.state.term.replace(/\s+/g, '');
+    let playLetterFns = [];
 
-    [...trimmedUppercasedTerm].forEach((char, i) => {
-      let audio = new Audio(`${process.env.PUBLIC_URL}/${char}.wav`);
+    [...trimmedTerm].forEach((char, i) => {
+      let audio = new Audio(`${process.env.PUBLIC_URL}/${char.toUpperCase()}.wav`);
 
-      setTimeout(() => audio.play(), (i + 1) * 1000);
-    })
+      let playLetterFn = new Promise( resolve => {
+        setTimeout(() => {
+          audio.play();
+
+          this.setState({ currentLetter: char });
+          resolve();
+        }, (i + 1) * 1000);
+      });
+
+      playLetterFns.push(playLetterFn);
+    });
+
+    await Promise.all(playLetterFns);
+
+    this.setState({ currentLetter: this.state.term })
   }
 
   handleInputChange = event => {
@@ -22,15 +36,16 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="h-screen bg-indigo-500">
-        <div className="w-full flex justify-center">
-          <form className="mt-12 p-12 w-8/12 flex flex-wrap justify-center" onSubmit={this.handleMe}>
-            <input className="border-b-8 border-gray-800 bg-indigo-500 w-full py-12 px-12 mb-12 text-5xl text-white placeholder-white focus:outline-none" val={this.term} placeholder="Type your word here" onChange={this.handleInputChange} />
-            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-24 text-2xl rounded">
-              Spell
-            </button>
-          </form>
+      <div className="w-full flex flex-wrap justify-center">
+        <div className="w-full flex justify-center h-48">
+          <span className="block-inline text-6xl font-bold text-white p-12">{this.state.currentLetter}</span>
         </div>
+        <form className="mt-6 p-12 w-8/12 flex flex-wrap justify-center" onSubmit={this.handleMe}>
+          <input className="border-b-8 border-gray-800 bg-indigo-500 w-full py-12 px-12 mb-12 text-5xl text-white placeholder-white focus:outline-none" val={this.term} placeholder="Type your word here" onChange={this.handleInputChange} />
+          <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-24 text-2xl rounded">
+            Spell
+          </button>
+        </form>
       </div>
     )
   }
